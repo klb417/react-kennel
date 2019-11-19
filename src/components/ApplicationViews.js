2a63d688bdf52753ac5ee1c8267f2ab4143823d1
Ken Boyd <klb417@gmail.com>
2019-11-19 11:29:27 -0600

    added animal list that creates animal card

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,23 +1,23 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
-import AnimalCard from './animal/AnimalCard'
-//only include these once they are built - previous practice exercise
+
 import LocationCard from './location/LocationCard'
 import EmployeeCard from './employee/EmployeeCard'
 import OwnerCard from './owner/OwnerCard'
+import AnimalList from './animal/AnimalList'
 
 
 class ApplicationViews extends Component {
 
   render() {
     return (
-      <React.Fragment>
+      <>
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
         <Route path="/animals" render={(props) => {
-          return <AnimalCard />
+          return <AnimalList />
         }} />
         <Route path="/locations" render={(props) => {
           return <LocationCard />
@@ -28,7 +28,7 @@ class ApplicationViews extends Component {
         <Route path="/owners" render={(props) => {
           return <OwnerCard />
         }} />
-      </React.Fragment>
+      </>
     )
   }
 }
